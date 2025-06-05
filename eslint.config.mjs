141a8210e58a40746b2cb55e1ec5ff0487aac2fd
kekekuli <kekekuliobj@gmail.com>
2025-06-06 01:16:36 +0800

    feat: add lint rules with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals", "next/typescript", "prettier"
+    ],
+    rules: {
+      semi: ["warn"],
+      indent: ["warn", 2],
+      "no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 0 }],
+      "no-mixed-spaces-and-tabs": "warn",
+    }
+  }),
 ];
 
 export default eslintConfig;
